d1e29bd160fa1372b8ad7072354e33d2e256beb6
丰羿 王 <tra202temu2000@gmail.com>
2018-05-09 16:44:01 +0800

    加入專案檔案。

---
+++ Models/Products.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildSchool.MvcSolution.OnlineStore.Models
+{
+    public class Products
+    {
+        public int ProductID { get; set; }
+        public string Category { get; set; }
+        public string Name { get; set; }
+        public int UnitPrice { get; set; }
+    }
+}
